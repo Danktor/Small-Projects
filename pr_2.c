8a56dd43927b30025de41387b68fa7c104774008
Diego <diegoforlongnell@gmail.com>
2023-12-26 18:08:30 -0300

    Project modified

--- pr_2.c
+++ pr_2.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
 int main(void){
-	int r = 10;
+	int r;
+	printf("Enter the radius: ");
+	scanf("%d", &r);	
 	int volume = (4.0f/3.0f)*3.14*r*r*r;
 	printf("The volume is: %d\n", volume);
 	return 0;
