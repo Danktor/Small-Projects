189f25a052677d22dc24b6aea8082b0104767ee0
Diego <diegoforlongnell@gmail.com>
2023-12-27 11:02:03 -0300

    bills amount (added pointers)

--- pr_5.c
+++ pr_5.c
@@ -1,17 +1,26 @@
 #include<stdio.h>
 
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones);
+
 int main(void){
-	int amount,twenty,ten,five,one;
+	int amount;
+	int twenty = 0; int ten = 0; int five = 0; int one = 0;
+	int *twenties = &twenty; int *tens = &ten; int *fives = &five; int *ones = &one;
 	printf("Enter a US dollar amount: ");
 	scanf("%d",&amount);
-	twenty = amount/20;
-	ten = (amount-twenty*20)/10;
-	five = (amount-twenty*20-ten*10)/5;
-	one = (amount-twenty*20-ten*10-five*5)/1;
-	
+	pay_amount(amount,twenties,tens,fives,ones);	
 	printf("$20 bills: %d\n",twenty);
 	printf("$10 bills: %d\n",ten);
 	printf("$5 bills: %d\n",five);
 	printf("$1 bills: %d\n",one);
 	return 0;
+}
+
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones){
+	*twenties = dollars/20;
+	*tens = (dollars-*twenties*20)/10;
+	*fives = (dollars-*twenties*20-*tens*10)/5;
+	*ones = (dollars-*twenties*20-*tens*10-*fives*5)/1;
+	
+	
 }
